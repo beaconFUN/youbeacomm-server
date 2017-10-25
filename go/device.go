68931128029126bc97a7185ca5b097357b9116d6
Daisuke Takahashi <daisuke@extendwings.com>
2017-10-25 17:33:34 +0900

    Improve error handlings

--- go/device.go
+++ go/device.go
@@ -15,8 +15,18 @@ type Device struct {
 func DevicePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
+	if r.Header.Get("Content-Length") == "0" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	var body Device
 	json.NewDecoder(r.Body).Decode(&body)
+
+	if body.OSName == "" || body.OSVersion == "" || body.OSLang == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(uuid.NewV4().String())
