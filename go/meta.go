ef976df707e60ba1f70304d6543dcf155afb6529
Daisuke Takahashi <daisuke@extendwings.com>
2017-10-25 17:45:52 +0900

    Implement /meta/

--- go/meta.go
+++ go/meta.go
@@ -1,13 +1,23 @@
 package youbeacomm
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 type Meta struct {
+	APIVersions []string `json:"available_api_versions"`
+}
+
+var api_versions = []string{
+	"1.0.0-alpha",
+	"1.0.0-alpha.2017092905",
+	"1.0.0-alpha.2017103001",
 }
 
 func RootGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(Meta{api_versions})
 }
